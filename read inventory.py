e056103b48ada85de8f204bae98d4b5b9c357182
Elias <123315404+Elwii04@users.noreply.github.com>
2025-04-21 18:15:21 +0200

    Delete read inventory.py

--- read inventory.py
+++
@@ -1,6 +0,0 @@
-import pytesseract
-
-my_tesseract_config = r"--psm 7 --oem 3"
-
-inventory_screenshot = pytesseract.image_to_string("Pictures\Test1.PNG", config=my_tesseract_config)
-print(inventory_screenshot)
